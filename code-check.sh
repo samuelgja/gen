c9d4f40d7d2b8397b657c7c00bfd169fb22f25ac
samuelgja <samuel.gjabel@gmail.com>
2023-12-06 15:25:54 +0100

    feat: update code-chek

--- code-check.sh
+++ code-check.sh
@@ -4,6 +4,5 @@ cargo fmt; # format the code
 cargo clippy --fix --allow-dirty;   # check the code
 cargo check; # check the code
 cargo test --release; # run the tests
-cargo machete # prints unused libraries
 # Build is not needed as it is inside custom build CI/CD pipeline
 # cargo build --release; # check build --release
