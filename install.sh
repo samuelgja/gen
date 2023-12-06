6166c9bc5c54343f35b18e41e387005e9fb0a80a
samuelgja <samuel.gjabel@gmail.com>
2023-12-06 14:57:22 +0100

    feat: update install script

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# Script URL: https://raw.githubusercontent.com/samuelgja/gen/main/install.sh
+
 # Gen Installation Script
 
-echo "Downloading Gen from https://github.com/samuelgja/gen/releases..."
+echo "Installing Gen..."
 
 # Specify the URL to the latest release of Gen
-GEN_RELEASE_URL="https://github.com/samuelgja/gen/releases/latest/download/gen"
+GEN_BINARY="https://github.com/samuelgja/gen/raw/main/releases/latest/download/gen"
 
 # Download the Gen binary
-curl -L $GEN_RELEASE_URL -o gen
+curl -L $GEN_BINARY -o gen
 
-# Make the binary executable
-chmod +x gen
+# # Make the binary executable
+# chmod +x gen
 
-# Move the binary to a directory in the user's PATH
-# /usr/local/bin is a common directory for user-installed binaries
-mv gen /usr/local/bin/
+# # Move the binary to a directory in the user's PATH
+# # /usr/local/bin is a common directory for user-installed binaries
+# mv gen /usr/local/bin/
 
-echo "Gen has been installed successfully."
-echo "Type 'gen --help' to see available commands."
+# echo "Gen has been installed successfully."
+# echo "Type 'gen --help' to see available commands."
